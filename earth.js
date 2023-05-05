5aa07902decc23c478a65f73f7e8b2fbdd4cdc97
darthphobos <130782298+darthphobos@users.noreply.github.com>
2023-05-05 11:25:18 -0600

    Add files via upload

---
+++ earth.js
@@ -0,0 +1,21 @@
+//functionaltiy for earth info
+document.getElementById('earth-button').addEventListener('click', () => document.getElementById('earth-info').style.display = 'block')
+document.getElementById('earth-button').addEventListener('dblclick', () => document.getElementById('earth-info').style.display = 'none')
+
+document.getElementById('igneous-button').addEventListener('click', () => document.getElementById('igneous-info').style.display = 'block')
+document.getElementById('igneous-button').addEventListener('dblclick', () => document.getElementById('igneous-info').style.display = 'none')
+
+document.getElementById('metamorphic-button').addEventListener('click', () => document.getElementById('metamorphic-info').style.display = 'block')
+document.getElementById('metamorphic-button').addEventListener('dblclick', () => document.getElementById('metamorphic-info').style.display = 'none')
+
+document.getElementById('sedimentary-button').addEventListener('click', () => document.getElementById('sedimentary-info').style.display = 'block')
+document.getElementById('sedimentary-button').addEventListener('dblclick', () => document.getElementById('sedimentary-info').style.display = 'none')
+
+document.getElementById('volcano-button').addEventListener('click', () => document.getElementById('volcano-info').style.display = 'block')
+document.getElementById('volcano-button').addEventListener('dblclick', () => document.getElementById('volcano-info').style.display = 'none')
+
+document.getElementById('mineral-button').addEventListener('click', () => document.getElementById('mineral-info').style.display = 'block')
+document.getElementById('mineral-button').addEventListener('dblclick', () => document.getElementById('mineral-info').style.display = 'none')
+
+document.getElementById('biosphere-button').addEventListener('click', () => document.getElementById('biosphere-info').style.display = 'block')
+document.getElementById('biosphere-button').addEventListener('dblclick', () => document.getElementById('biosphere-info').style.display = 'none')
