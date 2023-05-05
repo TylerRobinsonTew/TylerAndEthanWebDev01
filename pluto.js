a6765094b2dec6ceadcf758b78d6d900122cd19c
darthphobos <130782298+darthphobos@users.noreply.github.com>
2023-05-05 11:26:05 -0600

    Add files via upload

---
+++ pluto.js
@@ -0,0 +1,17 @@
+document.getElementById('pluto-button').addEventListener('click', () => document.getElementById('pluto-info').style.display = 'block')
+document.getElementById('pluto-button').addEventListener('dblclick', () => document.getElementById('pluto-info').style.display = 'none')
+
+document.getElementById('charon-button').addEventListener('click', () => document.getElementById('charon-info').style.display = 'block')
+document.getElementById('charon-button').addEventListener('dblclick', () => document.getElementById('charon-info').style.display = 'none')
+
+document.getElementById('styx-button').addEventListener('click', () => document.getElementById('styx-info').style.display = 'block')
+document.getElementById('styx-button').addEventListener('dblclick', () => document.getElementById('styx-info').style.display = 'none')
+
+document.getElementById('nix-button').addEventListener('click', () => document.getElementById('nix-info').style.display = 'block')
+document.getElementById('nix-button').addEventListener('dblclick', () => document.getElementById('nix-info').style.display = 'none')
+
+document.getElementById('kerberos-button').addEventListener('click', () => document.getElementById('kerberos-info').style.display = 'block')
+document.getElementById('kerberos-button').addEventListener('dblclick', () => document.getElementById('kerberos-info').style.display = 'none')
+
+document.getElementById('hydra-button').addEventListener('click', () => document.getElementById('hydra-info').style.display = 'block')
+document.getElementById('hydra-button').addEventListener('dblclick', () => document.getElementById('hydra-info').style.display = 'none')
