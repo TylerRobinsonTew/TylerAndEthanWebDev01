56bc52ed243438225882bbe73a82273ac95a75d4
darthphobos <130782298+darthphobos@users.noreply.github.com>
2023-05-05 11:21:35 -0600

    Create plants.js

---
+++ plants.js
@@ -0,0 +1,35 @@
+// functionality for redwood info
+document.getElementById('redwood-button').addEventListener('click', () => document.getElementById('redwood-info').style.display = 'block')
+document.getElementById('redwood-button').addEventListener('dblclick', () => document.getElementById('redwood-info').style.display = 'none')
+
+// functionality for sequoias info
+document.getElementById('sequoia-button').addEventListener('click', () => document.getElementById('sequoia-info').style.display = 'block')
+document.getElementById('sequoia-button').addEventListener('dblclick', () => document.getElementById('sequoia-info').style.display = 'none')
+
+//functionality for cherryblossoms info
+document.getElementById('cherry-button').addEventListener('click', () => document.getElementById('cherry-info').style.display = 'block')
+document.getElementById('cherry-button').addEventListener('dblclick', () => document.getElementById('cherry-info').style.display = 'none')
+
+//functionality for sunflowers info
+document.getElementById('sun-button').addEventListener('click', () => document.getElementById('sun-info').style.display = 'block')
+document.getElementById('sun-button').addEventListener('dblclick', () => document.getElementById('sun-info').style.display = 'none')
+
+//functionality for tulips info
+document.getElementById('tulip-button').addEventListener('click', () => document.getElementById('tulip-info').style.display = 'block')
+document.getElementById('tulip-button').addEventListener('dblclick', () => document.getElementById('tulip-info').style.display = 'none')
+
+//functionality for dandilions info
+document.getElementById('dandilion-button').addEventListener('click', () => document.getElementById('dandilion-info').style.display = 'block')
+document.getElementById('dandilion-button').addEventListener('dblclick', () => document.getElementById('dandilion-info').style.display = 'none')
+
+//functionality for leatherleaf info
+document.getElementById('leather-button').addEventListener('click', () => document.getElementById('leather-info').style.display = 'block')
+document.getElementById('leather-button').addEventListener('dblclick', () => document.getElementById('leather-info').style.display = 'none')
+
+//functionality for cocanuttree info
+document.getElementById('cocanut-button').addEventListener('click', () => document.getElementById('cocanut-info').style.display = 'block')
+document.getElementById('cocanut-button').addEventListener('dblclick', () => document.getElementById('cocanut-info').style.display = 'none')
+
+//functionality for eaglefern info
+document.getElementById('eaglefern-button').addEventListener('click', () => document.getElementById('eaglefern-info').style.display = 'block')
+document.getElementById('eaglefern-button').addEventListener('dblclick', () => document.getElementById('eaglefern-info').style.display = 'none')
